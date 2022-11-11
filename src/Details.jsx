b4b88352c21a23f35439415ae8e326c4a472806e
Mccjoey <crash.oliver@gmail.com>
2022-11-11 16:34:49 -0300

    Part 5 - Error Boundary

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
@@ -30,4 +31,11 @@ const Details = () => {
   );
 };
 
-export default Details;
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
